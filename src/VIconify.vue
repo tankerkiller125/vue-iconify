a332c9c395447c62117ac2f8201949a0c9e5153b
Matthew Kilgore <tankerkiller125@gmail.com>
2019-07-23 21:52:45 -0400

    Start from scratch again basically

---
+++ src/VIconify.vue
@@ -0,0 +1,32 @@
+<template>
+    <span>{{ icon.body }}</span>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            icon: {
+                body: null
+            }
+        }),
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            inline: Boolean
+        },
+        created() {
+            var icons = this.name.toString().split(':');
+            fetch(`https://api.iconify.design/${icons[0]}.json?icons=${icons[1]}`).then((response) => {
+               return response.json().then((json) => {
+                   this.icon.body = json.icons[0].body
+               })
+            });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
